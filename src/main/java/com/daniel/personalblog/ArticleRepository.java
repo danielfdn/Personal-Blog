fca233936e8d4032f79862494342e1b146640010
Daniel Fandin <danielfandin28@gmail.com>
2026-03-11 09:02:32 +0100

    Added a PostgreSQL database with Spring Data JPA. Schema definition: /src/main/java/resources/schema.sql

--- src/main/java/com/daniel/personalblog/ArticleRepository.java
+++ src/main/java/com/daniel/personalblog/ArticleRepository.java
@@ -3,6 +3,4 @@ package com.daniel.personalblog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-
-}
+public interface ArticleRepository extends JpaRepository<Article, Long> {}
--- src/main/java/com/daniel/personalblog/BlogController.java
+++ src/main/java/com/daniel/personalblog/BlogController.java
@@ -3,9 +3,7 @@ package com.daniel.personalblog;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-
+import java.util.List;
 
 
 @Controller
@@ -19,39 +17,39 @@ public class BlogController {
 
     @GetMapping({"","/","/home"})
     public String homeView(Model model) { //model aus MVC - enthält Daten, die von der Ansicht dargestellt werden// (=model view controller)
-        ArrayList<Article> articleList = articleService.findAll();
+        List<Article> articleList = articleService.findAll();
         model.addAttribute("articleList", articleList);
         return "home"; //Name unserer html Datei
     }
 
-    @GetMapping("/article/{idx}")
-    public String articleDetail(@PathVariable int idx, Model model) {
-        Article article = articleService.findByIndex(idx);
+    @GetMapping("/article/{id}")
+    public String articleDetail(@PathVariable Long id, Model model) {
+        Article article = articleService.findById(id);
         model.addAttribute("article", article);
         return "article";
     }
 
     @GetMapping("/admin")
-    public String adminView(Model model, Integer idx) {
-        ArrayList<Article> articleList = articleService.findAll();
+    public String adminView(Model model, Long id) {
+        List<Article> articleList = articleService.findAll();
         model.addAttribute("articleList", articleList);
         return "admin-view";
     }
 
-    @GetMapping("/edit/{idx}") //+ postMapper
-    public String editArticle(@PathVariable int idx, Model model) {
-        Article article = articleService.findByIndex(idx);
+    @GetMapping("/edit/{id}") //+ postMapper
+    public String editArticle(@PathVariable Long id, Model model) {
+        Article article = articleService.findById(id);
 
         model.addAttribute("article", article);
-        model.addAttribute("idx", idx);
+        model.addAttribute("id", id);
 
 
         return "update-article";
     }
 
-    @PostMapping("/edit/{idx}")
-    public String postEditedArticle(@PathVariable int idx , @ModelAttribute Article article) {
-        articleService.editByIndex(idx, article);
+    @PostMapping("/edit/{id}")
+    public String postEditedArticle(@PathVariable Long id, @ModelAttribute Article article) {
+        articleService.editById(id, article);
         return "redirect:/admin";
     }
 
@@ -69,12 +67,11 @@ public class BlogController {
         return "redirect:/admin";
     }
 
-    @PostMapping("/admin/delete/{idx}")
-    public String deleteArticle(@PathVariable int idx){
-        articleService.deleteByIndex(idx);
+    @PostMapping("/admin/delete/{id}")
+    public String deleteArticle(@PathVariable Long id){
+        articleService.deleteById(id);
         return "redirect:/admin";
     }
-
 }
 
 
